14a0dda47fabc4cefbaf48776a28c46fd8e18d5a
Lizi <86664792+whooslizi@users.noreply.github.com>
2026-08-03 03:40:14 +0700

    fix: simplify vercel.json to standard rewrites for automatic Node File Trace bundling

--- addon.js
+++ addon.js
@@ -12,13 +12,7 @@ const {
 } = require('./languages');
 const { alignAndMatch } = require('./lib/syncEngine');
 const { generateCandidatePairs, filterByLanguage } = require('./lib/sourceSelection');
-let scrapersModule;
-try {
-  scrapersModule = require('./scrapers/index');
-} catch (e) {
-  scrapersModule = require('./scrapers');
-}
-const { scrapeAllSources, generateSelectableDualPairs } = scrapersModule;
+const { scrapeAllSources, generateSelectableDualPairs } = require('./scrapers/index');
 const { singleflight } = require('./lib/singleflight');
 const { translateSubtitleCues, isTranslationEnabled } = require('./lib/translator');
 
